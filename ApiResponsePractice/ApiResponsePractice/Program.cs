e7ddd2ae1727f5874f5c29605c68c08b06938792
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2023-12-04 12:56:31 +0000

    API connected
    API connected

---
+++ ApiResponsePractice/ApiResponsePractice/Program.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Net;
+using System.Text.Json;
+﻿using System.Net.Http;
+
+
+class Program
+{
+	public static async Task BoredSearchAsync()
+	{
+		while (true)
+		{ 
+		Console.WriteLine("Find an activity to stop the boredom! ");
+		Console.WriteLine("Please choose from: education, recreational, social, diy, charity, cooking, relaxation, music or busywork");
+
+		string enteredType = Console.ReadLine();
+			Console.WriteLine("\n ");
+
+			if (!IsValidInput(enteredType))
+		{
+			Console.WriteLine("Please enter a valid activity from the provided list.");
+			return;
+		}
+			try
+		{
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri("http://www.boredapi.com/api/activity/");
+
+					var response = await client.GetAsync($"?type={enteredType}");
+
+					if (response.IsSuccessStatusCode)
+				{
+					var responseContent = await response.Content.ReadAsStringAsync();
+					var getResponse = JsonSerializer.Deserialize<GetResponse>(responseContent);
+
+					if (string.IsNullOrEmpty(enteredType))
+					{
+						Console.WriteLine("Please choose from one below");
+						Console.WriteLine(getResponse.count);
+
+						foreach (var bored in getResponse.results)
+						{
+							Console.WriteLine("Name: " + bored.activity);
+							Console.WriteLine("URL: " + bored.url);
+
+							Console.WriteLine("---------------------");
+						}
+					}
+					else
+					{
+						Console.WriteLine($"Activity: {getResponse.activity}");
+						Console.WriteLine($"Accessibility: {getResponse.accessibility}");
+						Console.WriteLine($"Type: {getResponse.type}");
+						Console.WriteLine($"Participants: {getResponse.participants}");
+						Console.WriteLine($"Price: £{getResponse.price}");
+					}
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error: {ex.Message}");
+		}
+			Console.WriteLine("Do you want to search again? (yes/no)");
+			string restartChoice = Console.ReadLine().ToLower();
+
+			if (restartChoice != "yes")
+			{
+				break; 
+			}
+		}
+	}
+
+	private static bool IsValidInput(string enteredName)
+	{
+		string[] validInputs = { "education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork" };
+		return validInputs.Contains(enteredName.ToLower());
+	}
+
+	static async Task Main(string[] args)
+	{
+		await BoredSearchAsync();
+	}
+}
---
+++ ApiResponsePractice/ApiResponsePractice/src/GetResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+	public class GetResponse
+	{
+		public int count { get; set; }
+		public List<NamedApiResource> results { get; set; }
+		public string activity { get; set; }
+		public double accessibility { get; set; }
+		public string type { get; set; }
+		public int participants { get; set; }
+		public double price { get; set; }
+		
+
+
+}
